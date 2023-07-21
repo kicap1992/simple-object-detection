d9fc39122be73765d2dc126278fde695167f825e
kicap92 <kicap.karan92@gmail.com>
2023-07-21 12:00:41 +0800

    hehe

--- main.py
+++ main.py
@@ -41,55 +41,54 @@ rows, cols, _ = frame.shape
 x_medium = int(cols / 2)
 center = int(cols / 2)
 
-if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-    ser.flush()
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+ser.flush()
 
-    while True:
-        ret, frame = cap.read()
-        low_blue = np.array([100, 150, 0])
-        high_blue = np.array([140, 255, 255])
+while True:
+    ret, frame = cap.read()
+    low_blue = np.array([100, 150, 0])
+    high_blue = np.array([140, 255, 255])
 
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv, low_blue, high_blue)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, low_blue, high_blue)
 
-        
+    
 
-        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        contours = sorted(contours, key=lambda x:cv2.contourArea(x), reverse=True)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=lambda x:cv2.contourArea(x), reverse=True)
 
-        for cnt in contours:
-            (x, y, w, h) = cv2.boundingRect(cnt)
-            x_medium = int((x + x + w) / 2)
-            break
+    for cnt in contours:
+        (x, y, w, h) = cv2.boundingRect(cnt)
+        x_medium = int((x + x + w) / 2)
+        break
 
-        cv2.line(frame, (x_medium, 0), (x_medium, 480), (0, 255, 0), 2)
-        # cv2.imshow("Mask", mask)
-        cv2.imshow("Frame", frame)
+    cv2.line(frame, (x_medium, 0), (x_medium, 480), (0, 255, 0), 2)
+    # cv2.imshow("Mask", mask)
+    cv2.imshow("Frame", frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-        if x_medium < center  - 25:
-            # position += 1
-            # if position > 180:
-            #     position = 180
-            # SetAngle(position)
-            ser.write(b'right\n')
-            # time.sleep(0.5)
-            print("Servo is moving to the right")
-        elif x_medium > center + 25:
-            # position -= 1
-            # if position < 0:
-            #     position = 0
-            # SetAngle(position)
-            ser.write(b'left\n')
-            # time.sleep(0.5)
-            print("Servo is moving to the left")
-        else:
-            ser.write(b'stop\n')
-            # time.sleep(0.5)
-            print("Servo is not moving")
+    if x_medium < center  - 25:
+        # position += 1
+        # if position > 180:
+        #     position = 180
+        # SetAngle(position)
+        ser.write(b'right\n')
+        # time.sleep(0.5)
+        print("Servo is moving to the right")
+    elif x_medium > center + 25:
+        # position -= 1
+        # if position < 0:
+        #     position = 0
+        # SetAngle(position)
+        ser.write(b'left\n')
+        # time.sleep(0.5)
+        print("Servo is moving to the left")
+    else:
+        ser.write(b'stop\n')
+        # time.sleep(0.5)
+        print("Servo is not moving")
 
             
 
